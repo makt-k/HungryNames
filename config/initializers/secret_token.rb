b1065ec01770f92c3045e5f310547c4f2f6f2afd
K Mak <karen.mak@stx.oxon.org>
2014-04-26 19:20:23 -0400

    initial commit of new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hungrynames::Application.config.secret_key_base = '62d5377f73f1db95c0548a302b89ae31e405b6e21c6d32f254cc54a44e8af310d8861204447236b885b8b03e9f45b2e9d82afee76b7c0b268bc5185fe0907e0d'
